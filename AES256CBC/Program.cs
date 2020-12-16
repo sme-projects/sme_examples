9b03ea456cf42cd1c22ae291d08b6a6fbfd24078
Carl Johnsen <cjjohnsen@nbi.ku.dk>
2020-12-16 02:38:24 +0100

    Added AES example

---
+++ AES256CBC/Program.cs
@@ -0,0 +1,26 @@
+﻿using SME;
+
+namespace AES256CBC
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+            using (var sim = new Simulation())
+            {
+                var core = new AESCore();
+                var test = new Tester();
+
+                core.Input = test.Input;
+                test.Output = core.Output;
+
+                sim
+                    .AddTopLevelInputs(core.Input)
+                    .AddTopLevelOutputs(core.Output)
+                    .BuildCSVFile()
+                    .BuildVHDL()
+                    .Run();
+            }
+		}
+	}
+}
